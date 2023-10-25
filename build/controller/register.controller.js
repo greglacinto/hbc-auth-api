5fa95b60a05dc59e0a2360f82fb4ae8547b3cd58
Gregory Odiase <49419566+greglacinto@users.noreply.github.com>
2023-10-25 08:48:22 +0100

    Delete build directory

--- build/controller/register.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Register = void 0;
-function Register(req, res) {
-    const { email, password } = req.body;
-    console.log(email, password);
-    return res.status(200).send({ "status": 200, "message": "Register endpoint is up" });
-}
-exports.Register = Register;
